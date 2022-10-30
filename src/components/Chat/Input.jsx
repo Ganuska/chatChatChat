01f58ee2c4120330a7660fb9fa3769bc5be309bc
Zoran <101511908+Ganuska@users.noreply.github.com>
2022-10-30 13:16:11 +0100

    added more
    added message style

--- src/components/Chat/Input.jsx
+++ src/components/Chat/Input.jsx
@@ -1,16 +1,24 @@
 import React from "react";
-
 const Input = () => {
   return (
     <div className="flex w-full relative items-center">
       <input
         type="text"
         placeholder="Type something..."
-        className=" outline-none w-full border-none p-3  cursor-pointer box-border h-[50px]"
+        className=" outline-none w-full border-none p-3  cursor-text box-border h-[50px]"
       />
-      <button className="absolute right-3 bg-slate-800 p-1 text-white rounded-sm ">
+      <button className="absolute right-3 bg-slate-800 p-1 text-white rounded-md ">
         send
       </button>
+      <label htmlFor="InputFile" className="cursor-pointer">
+        <img
+          htmlFor="InputFile"
+          src="https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg"
+          alt="Input file"
+          className="w-9 h-9 absolute right-16 top-2 "
+        />
+      </label>
+      <input type="file" id="InputFile" className="hidden  " />
     </div>
   );
 };
