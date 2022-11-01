217310d99f15933c10f0372c9ddaf99641ebacef
Zoran <101511908+Ganuska@users.noreply.github.com>
2022-11-01 19:53:49 +0100

    Update LoginPage.jsx
    update so it not rerenders constantly

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -15,6 +15,7 @@ const LoginPage = () => {
   const [user, loading, error] = useAuthState(auth);
   const navigate = useNavigate();
   useEffect(() => {
+    console.log(user);
     if (loading) {
       // maybe trigger a loading screen
       return;
@@ -34,7 +35,12 @@ const LoginPage = () => {
     <div className="h-screen flex justify-center items-center bg-indigo-200">
       <div className=" flex justify-center items-center flex-col bg-indigo-200 sm:bg-white rounded-md border-1 p-6 border-emerald-100 w-full h-full sm:w-[300px] sm:h-[400px] ">
         <h1 className="text-center font-bold">Moj chat App</h1>
-        <form className="flex flex-col gap-5 justify-center mt-10 items-center">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+          className="flex flex-col gap-5 justify-center mt-10 items-center"
+        >
           <input
             type="email"
             placeholder="email"
@@ -54,9 +60,9 @@ const LoginPage = () => {
           />
 
           <button
-            onClick={() =>
-              logInWithEmailAndPassword(login.email, login.password)
-            }
+            onClick={() => {
+              logInWithEmailAndPassword(login.email, login.password);
+            }}
             className=" justify-center rounded-md flex w-1/2 text-center p-2 bg-sky-500 hover:bg-sky-200  "
           >
             Sign In
